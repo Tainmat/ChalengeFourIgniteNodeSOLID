d80343fdb5999a0cd9259ba5f6eb493a381e85fb
Tainã Martins Pino <63248706+Tainmat@users.noreply.github.com>
2022-07-27 07:18:29 -0300

    Implementation of listAllUser features

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const {id} = request.headers
+    const user_id = String(id)
+
+    const all = this.listAllUsersUseCase.execute({user_id})
+
+    return response.json(all)
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id)
+
+    if(!user){
+      throw new Error('Access denied! Log in and try again.')
+    } else if(!user.admin){
+      throw new Error('Access denied! You need to be an administrator.')
+    }
+
+    const users = this.usersRepository.list()
+    return users
   }
 }
 
